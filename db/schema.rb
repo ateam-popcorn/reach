890cfc6fe21cf4cf443adba05f78d1666c6211b8
nyamadori <nyamadorig@gmail.com>
2015-10-24 00:05:04 +0900

    [issue #14] Create Meeting model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022210717) do
+ActiveRecord::Schema.define(version: 20151023115948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.integer  "research_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "meetings", ["research_id"], name: "index_meetings_on_research_id", using: :btree
 
   create_table "researches", force: :cascade do |t|
     t.string   "title"
@@ -45,4 +55,5 @@ ActiveRecord::Schema.define(version: 20151022210717) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "meetings", "researches"
 end
