ce4abc0d51d96a4649d264b75fcee331b828fd8a
nyamadori <nyamadorig@gmail.com>
2015-10-24 00:19:12 +0900

    [issue #16] Create UserMeeting model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023115948) do
+ActiveRecord::Schema.define(version: 20151023151614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20151023115948) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_meetings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "meeting_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_meetings", ["meeting_id"], name: "index_user_meetings_on_meeting_id", using: :btree
+  add_index "user_meetings", ["user_id"], name: "index_user_meetings_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -56,4 +66,6 @@ ActiveRecord::Schema.define(version: 20151023115948) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "meetings", "researches"
+  add_foreign_key "user_meetings", "meetings"
+  add_foreign_key "user_meetings", "users"
 end
