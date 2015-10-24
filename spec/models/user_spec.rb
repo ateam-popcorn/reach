0234f3a1121dc3d040707e40ffc6f947125c2f79
nyamadori <nyamadorig@gmail.com>
2015-10-24 21:34:54 +0900

    Add example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   subject(:user) { User.new }
 
   it { is_expected.to respond_to(:meetings) }
+  it { is_expected.to respond_to(:user_meetings) }
   it { is_expected.to respond_to(:notes) }
 end
