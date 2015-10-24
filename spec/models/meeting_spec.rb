5566b4365ac98dafb304baa9dee9b37a2560bf7b
nyamadori <nyamadorig@gmail.com>
2015-10-24 22:08:49 +0900

    Add an example to Meeting spec

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Meeting, type: :model do
   subject(:meeting) { Meeting.new }
 
+  it { is_expected.to respond_to(:research) }
   it { is_expected.to respond_to(:users) }
   it { is_expected.to respond_to(:user_meetings) }
   it { is_expected.to respond_to(:notes) }
