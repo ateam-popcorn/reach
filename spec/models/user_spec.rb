b6fc715d5da06db78288830f551e30c4f0274fef
nyamadori <nyamadorig@gmail.com>
2015-10-24 21:25:22 +0900

    Add User model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { User.new }
+
+  it { is_expected.to respond_to(:meetings) }
+  it { is_expected.to respond_to(:notes) }
+end
