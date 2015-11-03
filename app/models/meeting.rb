49fd9cf30ffeee9953eddb600fdb8455377d6266
nyamadori <nyamadorig@gmail.com>
2015-11-03 18:58:45 +0900

    Participantモデルに対応

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ActiveRecord::Base
   belongs_to :research
-  has_many :users
-  has_many :user_meetings
+  has_many :participants
+  has_many :users, through: :participants
   has_many :notes
 end
