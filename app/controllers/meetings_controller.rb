b01cc2fbb390ec2fda4f56113a85b020c5952fa5
nyamadori <nyamadorig@gmail.com>
2015-11-03 18:10:14 +0900

    会議の作成機能を実装　

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -18,4 +18,27 @@ class MeetingsController < ApplicationController
     @meeting = Meeting.find(params[:id])
     @research = Research.find(@meeting.research_id)
   end
+
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+
+    if @meeting.save
+      flash[:notice] = 'hogehoge'
+    else
+      flash[:error] = 'error'
+      redirect_to :back
+    end
+
+    redirect_to action: :index
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:start_at, :end_at)
+  end
 end
