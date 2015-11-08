a21a81a7024ba8cdadf2aac61e29865136875cfb
ueki05 <uekiyuta1991@gmail.com>
2015-11-08 08:08:42 +0000

    rev user mypage all

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,21 +1,6 @@
 class ResearchesController < ApplicationController
   def index
-    @participants = Participant.where(user_id: current_user.id)
-    
-    @reserved_meetings = Array.new
-    @participants.each do |p|
-      @reserved_meetings.push(Meeting.find(p.meeting_id))
-    end
-
-    @reserved_research = Array.new
-    @reserved_meetings.each do |r|
-      research = Research.find(r.research_id)
-      hash = {title: research.title, meeting_id: r.id, start_at: r.start_at}
-      @reserved_research.push(Hash[hash])
-    end
-
     @researches = Research.all
-    @meetings = Meeting.all
   end
 
   def show
