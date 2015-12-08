f9aa6dbf2a04ea9efbb2316eae94a4a01dd9499a
nyamadori <nyamadorig@gmail.com>
2015-12-08 15:57:50 +0900

    各属性にenumを追加　

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,73 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   before_save :set_age_from_birthday
 
+  enum sex: [:male, :female]
+  enum prefecture: [
+    :hokkaido,
+    :aomori,
+    :iwate,
+    :miyagi,
+    :akita,
+    :yamagata,
+    :fukushima,
+    :ibaraki,
+    :tochigi,
+    :gumma,
+    :saitama,
+    :chiba,
+    :tokyo,
+    :kanagawa,
+    :niigata,
+    :toyama,
+    :ishikawa,
+    :fukui,
+    :yamanashi,
+    :nagano,
+    :gifu,
+    :shizuoka,
+    :aichi,
+    :mie,
+    :shiga,
+    :kyoto,
+    :osaka,
+    :hyogo,
+    :nara,
+    :wakayama,
+    :tottori,
+    :shimane,
+    :okayama,
+    :hiroshima,
+    :yamaguchi,
+    :tokushima,
+    :kagawa,
+    :ehime,
+    :kochi,
+    :fukuoka,
+    :saga,
+    :nagasaki,
+    :kumamoto,
+    :oita,
+    :miyazaki,
+    :kagoshima,
+    :okinawa
+  ]
+  enum job: [
+    :employee,
+    :manager,
+    :exective,
+    :government,
+    :temporary,
+    :self,
+    :soho,
+    :farming,
+    :profession,
+    :part_time,
+    :homemaker,
+    :student,
+    :no_job,
+    :others
+  ]
+
   private
 
   def set_age_from_birthday
