584a12565535d1aeaaa89b1efad57b749e047c00
ueki05 <uekiyuta1991@gmail.com>
2015-12-13 06:06:38 +0000

    rev question routing

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,10 @@ Rails.application.routes.draw do
       end
     end
     resource :candidate, only: [:create]
+    resource :question
   end
 
   resource :profile, only: [:edit, :show, :update]
-
-  resource :question
 
   devise_for :users
   get 'home/index'
