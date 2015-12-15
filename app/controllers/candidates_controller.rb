60353e98d6aa0f79076199cce869e2b3104be5b9
ueki05 <uekiyuta1991@gmail.com>
2015-12-15 02:34:17 +0000

    add fields_for

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,13 @@
 class CandidatesController < ApplicationController
   def new
+    @research = Research.find_by(id: params[:research_id])
     @question = Question.find_by(research_id: params[:research_id])
     @candidate = Candidate.new
   end
 
   def create
+    @question = Question.find_by(research_id: params[:research_id])
+
     redirect_to researches_path
   end
 
--- app/models/research.rb
+++ app/models/research.rb
@@ -2,4 +2,5 @@ class Research < ActiveRecord::Base
   has_many :meetings
   has_many :questions
   has_many :candidates
+  accepts_nested_attributes_for :questions
 end
