c5c045e81b13914be3620a9547f641ecea146d8d
ueki05 <uekiyuta1991@gmail.com>
2015-12-15 04:02:26 +0000

    create good candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,8 +6,25 @@ class CandidatesController < ApplicationController
   end
 
   def create
-    @question = Question.find_by(research_id: params[:research_id])
+    @research = Research.find_by(id: params[:research_id])
+    @candidate = @research.candidates.build
+    params['research']['questions_attributes'].each do |_,q|
+      question = Question.find(q['id'])
+      p "hogehoge"
+      p question.pass_answer
+      p q['answer']
+      if question.pass_answer != q['answer'].to_i
+        @candidate.user_id = current_user.id
+        @candidate.status = 1
+        @candidate.save
+        redirect_to researches_path
+        return
+      end
+    end
 
+    @candidate.user_id = current_user.id
+    @candidate.status = 0
+    @candidate.save
     redirect_to researches_path
   end
 
